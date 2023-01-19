41fd888d140c5d1fa1d9d409bd8f4c29c0c37afd
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-19 15:03:31 +0100

    ADD correct_name METHOD TO NAMEABLE CLASS

--- nameable.rb
+++ nameable.rb
@@ -1,3 +1,5 @@
 class Nameable
-    
+    def correct_name
+        raise NotImplementedError, "The implementation of the method #{__method__} in the class #{self.class} was not successful!"
+    end
 end
