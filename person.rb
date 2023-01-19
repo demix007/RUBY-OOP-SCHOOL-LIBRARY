6178887fa940c40e5cdc4933aca2b3a51aa45b51
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-19 08:32:24 +0100

    ADD INITIALIZE METHOD AND CONSTRUCTORS TO PERSON CLASS

--- person.rb
+++ person.rb
@@ -1,4 +1,11 @@
 class Person
     attr_reader :id
     attr_accessor :name, :age
+  
+    def initialize(name = 'unknown', age, parent_permission: true)
+      @id = Random.random(1...10000)
+      @age = age
+      @name = name
+      @parent_permission = parent_permission
+    end
 end
