a402ddb79e1cf2d0a84f858506649f5770232324
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-19 16:48:31 +0100

    MAKE CHANGES RECOMMENDED BY THE REVIEWER

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,12 +1,7 @@
 require './base_decorator'
 
 class TrimmerDecorator < BaseDecorator
-  def initialize(nameable)
-    super()
-    @nameable = nameable
-  end
-
   def correct_name
-    super[0..9]
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
   end
 end
