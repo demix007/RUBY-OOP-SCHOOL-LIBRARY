ba2258628e5189d914b7eb7f17648c98e5d26530
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-19 08:35:44 +0100

    ADD INITIALIZE METHOD AND CONSTRUCTORS TO STUDENT CLASS

--- student.rb
+++ student.rb
@@ -1,5 +1,8 @@
 require './person'
 
 class Student < Person
-
+  def initialize(classroom, age, name = 'unknown', parent_permission: true)
+    @classroom = classroom
+    super(age, name, parent_permission: parent_permission)
+  end
 end
