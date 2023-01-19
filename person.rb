d8707162eda8f0c6261352b87d46e68e40e2b6b3
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-19 08:42:58 +0100

    fix linter errors

--- person.rb
+++ person.rb
@@ -1,21 +1,21 @@
 class Person
-    attr_reader :id
-    attr_accessor :name, :age
-  
-    def initialize(name = 'unknown', age, parent_permission: true)
-      @id = Random.random(1...10000)
-      @age = age
-      @name = name
-      @parent_permission = parent_permission
-    end
-  
-    private
-  
-    def is_of_age?
-      @age >= 18
-    end
-  
-    def can_use_service?
-      @age >= 18 || parent_permission == true
-    end
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(name = 'unknown', age, parent_permission: true)
+    @id = Random.random(1...10000)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  def can_use_service?
+    @age >= 18 || parent_permission == true
+  end
 end
