46f43f2e36093d95cd7557892a5f3250e71071d8
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-19 08:37:56 +0100

    ADD INITIALIZE METHOD AND CONSTRUCTORS TO TEACHER CLASS

--- teacher.rb
+++ teacher.rb
@@ -1,5 +1,8 @@
 require './person'
 
 class Teacher < Person
-
+  def initialize(specialization, age, name = 'unknown', parent_permission: true)
+    @specialization = specialization
+    super(age, name, parent_permission: parent_permission)
+  end
 end
