6614f15ebaf40b1782f1ca6dbf33cd78d8270848
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-20 09:06:16 +0100

    FIX LINTER ERRORS

--- book.rb
+++ book.rb
@@ -1,13 +1,13 @@
 class Book
-    attr_accessor :title, :author, :rental_details
-  
-    def initialize(title, author)
-      @title = title
-      @author = author
-      @rental_details = []
-    end
-  
-    def add_rental_details(date, person)
-      Rental.new(date, self, person)
-    end
+  attr_accessor :title, :author, :rental_details
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rental_details = []
+  end
+
+  def add_rental_details(date, person)
+    Rental.new(date, self, person)
+  end
 end
--- student.rb
+++ student.rb
@@ -1,7 +1,8 @@
 require './person'
 
 class Student < Person
-  attr_reader : classroom
+  attr_reader :classroom
+
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
