09679b2bf9273edb71dd2c04fe0c073720719354
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-20 08:44:22 +0100

    CREATE THE belongs-to RELATIONSHIP BETWEEN Rental and Person

--- rental.rb
+++ rental.rb
@@ -1,9 +1,11 @@
 class Rental
-  attr_accessor :date, :book
+  attr_accessor :date, :book, :person
 
   def initialize(date)
     @date = date
     @book = book
     book.rental_details << self
+    @person = person
+    person.rental_details << self
   end
 end
