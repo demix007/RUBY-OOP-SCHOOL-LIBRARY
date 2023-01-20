4a116f9ffef265b429e2cb5f20733d521eeea820
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-20 08:18:19 +0100

    CREATET THE belongs-to RELATIONSHIP BETWEEN CLASSROOM AND STUDENT

--- student.rb
+++ student.rb
@@ -1,6 +1,7 @@
 require './person'
 
 class Student < Person
+  attr_reader : classroom
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
@@ -8,5 +9,10 @@ class Student < Person
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
