e561e790b42a3022feb4e6f31fab1fbb4d2593f0
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-21 06:14:57 +0100

    ADD initialize METHOD TO App CLASS

--- app.rb
+++ app.rb
@@ -1,3 +1,12 @@
-class App
+require './book'
+require './teacher'
+require './student'
+require './rental'
 
+class App
+    def initialize
+        @book_list = []
+        @people_list = []
+        @rentals_list = []
+    end
 end
