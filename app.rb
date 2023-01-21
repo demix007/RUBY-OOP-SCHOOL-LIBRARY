ba32f04ff0e07886f3f8186a2306643d4c31c9f7
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-21 06:27:12 +0100

    ADD list_of_books METHOD TO LIST ALL BOOKS

--- app.rb
+++ app.rb
@@ -7,6 +7,17 @@ class App
     def initialize
         @book_list = []
         @people_list = []
-        @rentals_list = []
+        @rental_list = []
     end
+
+    def list_of_books
+        if @book_list.empty?
+          puts 'There are currently no books available'
+        else
+          @book_list.each do |book|
+            puts "#{book.title} written by #{book.author}"
+          end
+        end
+    end
+
 end
