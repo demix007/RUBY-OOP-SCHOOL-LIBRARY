703a7b607dc252f6a65ec4696575fbfb8690d5ac
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-24 19:13:15 +0500

    refactor the main.rb file and and into app.rb file

--- app.rb
+++ app.rb
@@ -125,6 +125,29 @@ class App
       7: Exit School Library Application
       "
   end
+
+  def options(*)
+    loop do
+      display_all
+      option = gets.chomp.to_i
+      case option
+      when 1
+        list_of_people
+      when 2
+        list_of_books
+      when 3
+        create_a_person
+      when 4
+        create_a_book
+      when 5
+        create_a_rental
+      when 6
+        list_rental_by_id
+      else
+        break
+      end
+    end
+  end
 end
 
 book = App.new
