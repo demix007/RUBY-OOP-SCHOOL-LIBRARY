c49eefac17e5a7a9b9b4250aaea24b5092c0a57c
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-24 15:02:14 +0100

    REFACTOR main.rb FILE

--- app.rb
+++ app.rb
@@ -111,6 +111,20 @@ class App
       end
     end
   end
+
+  def display_all
+    puts 'Welcome To The School Library Application'
+    puts ' Please choose an action you want to perform'
+    puts "
+      1: List all books.
+      2: List all people.
+      3: Create a person(Teacher or Student)
+      4: Create a book
+      5: Create a rental
+      6: List all rentals for a given person ID
+      7: Exit School Library Application
+      "
+  end
 end
 
 book = App.new
